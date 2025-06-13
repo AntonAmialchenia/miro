d44fcc4a5d45273067b7fed80ebdb57f1d7164f3
Антон <antonivan@senthy.com>
2025-06-13 16:49:59 +0300

    add api

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,5 +1,67 @@
+import { rqClient } from "@/shared/api/instance";
+import { CONFIG } from "@/shared/model/config";
+import { ROUTES } from "@/shared/model/routes";
+import { useQueryClient } from "@tanstack/react-query";
+import { href, Link } from "react-router-dom";
+
 function BoardsListPage() {
-  return <div>BoardsListPage</div>;
+  const queryClient = useQueryClient();
+
+  const boardsQuery = rqClient.useQuery("get", "/boards");
+  const createMutation = rqClient.useMutation("post", "/boards", {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(
+        rqClient.queryOptions("get", "/boards")
+      );
+    },
+  });
+  const deleteBoardMutation = rqClient.useMutation(
+    "delete",
+    "/boards/{boardId}",
+    {
+      onSettled: async () => {
+        await queryClient.invalidateQueries(
+          rqClient.queryOptions("get", "/boards")
+        );
+      },
+    }
+  );
+  return (
+    <div>
+      <h1>Board list {CONFIG.API_BASE_URL}</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          createMutation.mutate({
+            body: { name: formData.get("name") as string },
+          });
+        }}
+      >
+        <input type="text" name="name" />
+        <button type="submit" disabled={createMutation.isPending}>
+          Create
+        </button>
+      </form>
+      {boardsQuery.data?.map((board) => (
+        <div key={board.id}>
+          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+            {board.name}
+          </Link>
+          <button
+            disabled={deleteBoardMutation.isPending}
+            onClick={() =>
+              deleteBoardMutation.mutate({
+                params: { path: { boardId: board.id } },
+              })
+            }
+          >
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export const Component = BoardsListPage;
