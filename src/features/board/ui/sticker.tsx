5bf33f3225284eb7362da8510398e4e1fd6702d0
Антон <antonivan@senthy.com>
2025-07-17 13:32:45 +0300

    add auto size

--- src/features/board/ui/sticker.tsx
+++ src/features/board/ui/sticker.tsx
@@ -1,4 +1,6 @@
+import { cn } from "@/shared/lib/css";
 import clsx from "clsx";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export function Sticker({
   text,
@@ -33,17 +35,53 @@ export function Sticker({
       style={{ transform: `translate(${x}px, ${y}px)` }}
       onClick={onClick}
     >
-      {isEditing ? (
-        <input
-          type="text"
-          className="w-full h-full"
-          autoFocus
-          value={text}
-          onChange={(e) => onTextChange?.(e.target.value)}
-        />
-      ) : (
-        text
-      )}
+      <TextareaAutoSize
+        value={text}
+        onChange={(value) => onTextChange?.(value)}
+        isEditing={isEditing ?? false}
+      />
     </button>
+  );
+}
+
+function TextareaAutoSize({
+  value,
+  onChange,
+  isEditing,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+  isEditing?: boolean;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      const { scrollHeight, scrollWidth } = ref.current;
+      setHeight(scrollHeight);
+      setWidth(scrollWidth);
+    }
+  }, [value]);
+  return (
+    <div className="relative">
+      <div
+        ref={ref}
+        className={cn("whitespace-pre-wrap", !isEditing && "opacity-0")}
+      >
+        {value}
+      </div>
+      {isEditing && (
+        <textarea
+          autoFocus
+          className="absolute left-0 top-0 resize-none overflow-hidden focus:outline-none"
+          style={{ width: width + 2, height: height + 2 }}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      )}
+    </div>
   );
 }
